49371bbd9cf04e3ec1a9eac04e6427b402bbc345
sadcfreightlink <sadcfreightlink@gmail.com>
2025-08-07 00:52:40 +0000

    Fixes for deployment v8

--- services/finance/finance.py
+++ services/finance/finance.py
@@ -4,7 +4,7 @@ from typing import Optional
 from fastapi import HTTPException
 from sqlalchemy.orm import Session
 from enums import EquipmentType, Recurrence_Days, TrailerLength, TrailerType, TruckType
-from models.brokerage.finance import Contract, LateFeeRates, Invoice, FinancialAccounts, , Withdrawal_Request
+from models.brokerage.finance import Contract, LateFeeRates, Invoice, FinancialAccounts, Withdrawal_Request
 from models.spot_bookings.dedicated_lane_ftl_shipment import FTL_Lane, CarrierFinancialAccounts
 from models.spot_bookings.ftl_shipment import FTL_SHIPMENT
 from schemas.brokerage.finance import Withdrawal_Request
