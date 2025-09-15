7327a20652a8c2491f3e5557ee0db8d845263460
sadcfreightlink <sadcfreightlink@gmail.com>
2025-09-15 21:57:31 +0000

    broker get single ftl shipment

--- api/endpoints/dashboards/broker_dashboard/shipment_management/broker_spot_shipment_management.py
+++ api/endpoints/dashboards/broker_dashboard/shipment_management/broker_spot_shipment_management.py
@@ -92,7 +92,7 @@ def broker_access_get_dashboard_home_data(
             },
             "pickup": {
                 "origin": load.origin_city_province,
-                "appointment": f"{load.pickup_date}-{load.pickup_appointment}"
+                "appointment": f"{load.pickup_date}, {load.pickup_appointment}"
             },
             "dropoff": {
                 "destination": load.destination_city_province,
@@ -205,3 +205,151 @@ def broker_access_get_dashboard_home_data(
             "lane_exchanges": formatted_lane_exchanges
         }
     }
+
+@router.post("/broker-access/ftl-shipment/{id}")
+def broker_access_get_individual_ftl_shipment(
+    id: int,
+    db: Session = Depends(get_db),
+    current_user: dict = Depends(get_current_user)
+):
+    assert "company_id" in current_user, "Missing company_id in current_user"
+    company_id = current_user.get("company_id")
+
+    if not company_id:
+        raise HTTPException(
+            status_code=400,
+            detail="User does not belong to a company"
+        )
+
+    try:
+        shipment = db.query(FTL_SHIPMENT).filter(FTL_SHIPMENT.id == id).first()
+        consignor = db.query(Consignor).filter(Consignor.id == shipment.consignor_id).first()
+        broker_transaction = db.query(Brokers_Brokerage_Transactions).filter(Brokers_Brokerage_Transactions.shipment_id == shipment.id,
+                                                                            Brokers_Brokerage_Transactions.type == shipment.type).first()
+        carrier = db.query(Carrier).filter(Carrier.id == shipment.carrier_id).first()
+        vehicle = db.query(Vehicle).filter(Vehicle.id == shipment.vehicle_id).first()
+        driver = db.query(Driver).filter(Driver.id == shipment.driver_id).first()
+
+        pickup_facility = db.query(ShipmentFacility).filter_by(id=shipment.pickup_facility_id).first()
+        delivery_facility = db.query(ShipmentFacility).filter_by(id=shipment.delivery_facility_id).first()
+
+        pickup_contact = db.query(ContactPerson).filter_by(id=pickup_facility.contact_person).first() if pickup_facility else None
+        delivery_contact = db.query(ContactPerson).filter_by(id=delivery_facility.contact_person).first() if delivery_facility else None
+
+        return {
+            "shipment_details": {
+                "id": shipment.id,
+                "invoice_id": shipment.invoice_id,
+                "status": shipment.shipment_status,
+                "trip_status": shipment.trip_status,
+                "is_sub_shipment": shipment.is_subshipment,
+                "lane_id": shipment.dedicated_lane_id,
+                "shipment_type": shipment.type,
+                "trip_type": shipment.trip_type,
+                "load_type": shipment.load_type,
+                "required_truck_type": shipment.required_truck_type,
+                "required_equipment_type": shipment.equipment_type,
+                "required_trailer_type": shipment.trailer_type,
+                "required_trailer_length": shipment.trailer_length,
+                "minimum_weight_bracket": shipment.minimum_weight_bracket,
+                "origin_address": shipment.complete_origin_address,
+                "destination_address": shipment.complete_destination_address,
+                "pickup_date": shipment.pickup_date,
+                "priority_level": shipment.priority_level,
+                "customer_referece_number": shipment.customer_reference_number,
+                "shipment_weight": shipment.shipment_weight,
+                "commodity": shipment.commodity,
+                "temperature_control": shipment.temperature_control,
+                "hazardous_materials": shipment.hazardous_materials,
+                "minimum_git_cover_amount": shipment.minimum_git_cover_amount,
+                "minimum_liability_cover_amount": shipment.minimum_liability_cover_amount,
+                "packaging_quantity": shipment.packaging_quantity,
+                "packaging_type": shipment.packaging_type,
+                "pickup_number": shipment.pickup_number,
+                "delivery_number": shipment.delivery_number,
+                "pickup_notes": shipment.pickup_notes,
+                "delivery_notes": shipment.delivery_notes,
+                "distance": shipment.distance,
+                "estimated_transit_time": shipment.estimated_transit_time,
+                "route_preview_embed": shipment.route_preview_embed,
+            },
+
+            "consignor_information": {
+                "id": consignor.id,
+                "client_type": consignor.client_type,
+                "business_sector": consignor.business_sector,
+                "company_name": consignor.company_name,
+                "contact_person": consignor.contact_person_name,
+                "phone_number": consignor.phone_number,
+                "email": consignor.email,
+                "consignor_billable": broker_transaction.per_shipment_consignor_billable if broker_transaction else "N/A",
+                "broker_profit": (broker.transaction.per_shipment_consignor_billable - shipment.quote)
+            },
+
+            "carrier_information": {
+                "id": carrier.id if carrier else "N/A",
+                "carrier_name": f"SADC FREIGHTLINK Carrier-{carrier.id}" if carrier else "N/A",
+                "carrier_git_cover": carrier.git_cover_amount if carrier else "N/A",
+                "carrier_liability_cover_amount": carrier.liability_insurance_cover_amount if carrier else "N/A",
+    
+                "assigned_vehicle": {
+                    "id": vehicle.id if vehicle else "N/A",
+                    "make": vehicle.make if vehicle else "N/A",
+                    "model": vehicle.model if vehicle else "N/A",
+                    "year": vehicle.color if vehicle else "N/A",
+                    "license_plate": vehicle.license_plate if vehicle else "N/A",
+                    "vin": vehicle.vin if vehicle else "N/A",
+                    "vehicle_type": vehicle.type if vehicle else "N/A",
+                    "equipment_type": vehicle.equipment_type if vehicle else "N/A",
+                    "trailer_type": vehicle.trailer_type if vehicle else "N/A",
+                    "trailer_length": vehicle.trailer_length if vehicle else "N/A",
+                    "tare_weight": vehicle.tare_weight if vehicle else "N/A",
+                    "gvm_weight": vehicle.gvm_weight if vehicle else "N/A",
+                    "payload_capacity": vehicle.payload_capacity if vehicle else "N/A",
+                },
+
+                "assigned_driver": {
+                    "id": driver.id if driver else "N/A",
+                    "first_name": driver.first_name if driver else "N/A",
+                    "last_name": driver.last_name if driver else "N/A",
+                    "license_number": driver.license_number if driver else "N/A",
+                    "email": driver.email if driver else "N/A",
+                    "phone_number": driver.phone_number if driver else "N/A",
+                },
+
+                "financial": {
+                    "rate": shipment.quote,
+                    "rate_per_kilometer": (shipment.quote / shipment.distance),
+                    "rate_per_ton": (shipment.quote / shipment.minimum_weight_bracket),
+                    "distance": shipment.distance,
+                    "payment_terms": shipment.payment_terms,
+                    "invoice_due_date": shipment.invoice_due_date,
+                },
+
+            "pickup_facility": {
+                "facility_name": pickup_facility.name if pickup_facility else None,
+                "address": pickup_facility.address if pickup_facility else None,
+                "time_window": f"{pickup_facility.start_time} - {pickup_facility.end_time}",
+                "scheduling_type": pickup_facility.scheduling_type,
+                "contact_name": f"{pickup_contact.first_name} - {pickup_contact.last_name}" if pickup_contact else None,
+                "email": pickup_contact.email if pickup_contact else None,
+                "contact_phone": pickup_contact.phone_number if pickup_contact else None,
+                "notes": pickup_facility.facility_notes if pickup_facility else None,
+            } if pickup_facility else None,
+
+            "delivery_facility": {
+                "facility_name": delivery_facility.name if delivery_facility else None,
+                "address": delivery_facility.address if delivery_facility else None,
+                "time_window": f"{delivery_facility.start_time} - {delivery_facility.end_time}",
+                "scheduling_type": delivery_facility.scheduling_type,
+                "contact_name": f"{delivery_contact.first_name} - {delivery_contact.last_name}" if pickup_contact else None,
+                "email": delivery_contact.email if pickup_contact else None,
+                "contact_phone": delivery_contact.phone_number if delivery_contact else None,
+                "notes": delivery_facility.facility_notes if delivery_facility else None,
+            } if delivery_facility else None,
+
+            }
+    }
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
