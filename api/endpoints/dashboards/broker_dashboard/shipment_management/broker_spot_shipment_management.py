61e8219c3b13ddea9670368baece1c4a86bb0f37
sadcfreightlink <sadcfreightlink@gmail.com>
2025-09-25 00:07:20 +0000

    Broker FTL Lane fetch

--- api/endpoints/dashboards/broker_dashboard/shipment_management/broker_spot_shipment_management.py
+++ api/endpoints/dashboards/broker_dashboard/shipment_management/broker_spot_shipment_management.py
@@ -672,7 +672,7 @@ def broker_access_get_individual_ftl_contract_lane(
             } if pickup_facility else None,
 
             "delivery_facility": {
-                "facility_name": delivery_facility.address.name if delivery_facility else None,
+                "facility_name": delivery_facility.name if delivery_facility else None,
                 "address": delivery_facility.address if delivery_facility else None,
                 "time_window": f"{delivery_facility.start_time} - {delivery_facility.end_time}",
                 "scheduling_type": delivery_facility.scheduling_type,
