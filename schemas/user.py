054d4cbc3f5c566eb330aa6b4e31eee9a128ab36
sadcfreightlink <sadcfreightlink@gmail.com>
2025-10-29 21:57:24 +0000

    Upload POD logging v3

--- schemas/user.py
+++ schemas/user.py
@@ -5,7 +5,7 @@ from typing import Literal
 
 class UploadPODRequest(BaseModel):
     shipment_id: str
-    shipment_type: Literal["FTL", "POWER"]
+    shipment_type: str
     pod_link: str
 
 class UserCreate(BaseModel):
