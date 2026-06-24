08947b03ca1d08ee39f824f8f8548a9d59f202c8
sadcfreightlink <sadcfreightlink@gmail.com>
2026-06-24 14:57:08 +0000

    admin get client summary and users v1.18

--- api/endpoints/dashboards/carrier_dashboard/spot_loadboards.py
+++ api/endpoints/dashboards/carrier_dashboard/spot_loadboards.py
@@ -146,6 +146,107 @@ def get_individual_spot_ftl_loadboard_shipment(
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+@router.get("/public-spot/ftl-loadboard") #UnTested
+def get_all_public_spot_ftl_loadboard_loads(db: Session = Depends(get_db)):
+    try:
+        # Query all records from the "dedicated_lanes_loadboard" table
+        shipments = db.query(Ftl_Load_Board).filter(Ftl_Load_Board.status == "Available").all()
+
+        return {
+            "shipments": [{
+                "id": shipment.shipment_id,
+                "trip_type": shipment.trip_type,
+                "rate": shipment.shipment_rate,
+                "distance": shipment.distance,
+                "route_preview_embed": shipment.route_preview_embed,
+                "rate_per_kilometer": shipment.rate_per_km,
+                "origin": shipment.origin_city_province,
+                "pickup_date": shipment.pickup_date,
+                "pickup_appointment": shipment.pickup_appointment,
+                "destination": shipment.destination_city_province,
+                "eta_date": shipment.eta_date,
+                "eta_window": shipment.eta_window,
+                "required_truck_type": shipment.required_truck_type,
+                "equipment_type": shipment.equipment_type,
+                "trailer_type": shipment.trailer_type,
+                "trailer_length": shipment.trailer_length,
+                "minimum_weight_bracket": shipment.minimum_weight_bracket,
+                "commodity": shipment.commodity,
+                "hazardous_materials": shipment.hazardous_metarials
+            } for shipment in shipments]
+        }
+    except Exception as e:
+        return {"error": str(e)}
+
+@router.get("/public-spot/ftl-loadboard/{id}")
+def get_public_individual_spot_ftl_loadboard_shipment(
+    id: int,
+    db: Session = Depends(get_db),
+):
+    try:
+        shipment = db.query(Ftl_Load_Board).filter(Ftl_Load_Board.shipment_id == id).first()
+
+        if not shipment:
+            raise HTTPException(status_code=404, detail="Shipment not found")
+
+        return {
+            "shipment_details": {
+                "id": shipment.shipment_id,
+                "type": shipment.type,
+                "load_type": shipment.load_type,
+                "trip_type": shipment.trip_type,
+                "status": shipment.status,
+                "required_truck_type": shipment.required_truck_type,
+                "equipment_type": shipment.equipment_type,
+                "trailer_type": shipment.trailer_type if shipment.trailer_type else "N/A",
+                "trailer_length": shipment.trailer_length if shipment.trailer_length else "N/A",
+                "minimum_weight_bracket": shipment.minimum_weight_bracket,
+                "minimum_git_cover_amount": shipment.minimum_git_cover_amount,
+                "minimum_liability_cover_amount": shipment.minimum_liability_cover_amount,
+                "origin": shipment.origin_city_province,
+                "destination": shipment.destination_city_province,
+                "distance": shipment.distance,
+                "rate": shipment.shipment_rate,
+                "rate_per_km": shipment.rate_per_km,
+                "rate_per_ton": shipment.rate_per_ton,
+                "pickup_date": shipment.pickup_date,
+                "eta_data": shipment.eta_date,
+                "payment_terms": shipment.payment_terms,
+                "payment_date": shipment.payment_date,
+                "minimum_transit_time": shipment.estimated_transit_time,
+                "route_preview": shipment.route_preview_embed,
+                "commodity": shipment.commodity,
+                "temperature_control": shipment.temperature_control,
+                "hazardous_materails": shipment.hazardous_metarials,
+                "packaging_quantity": shipment.packaging_quantity,
+                "packaging_type": shipment.packaging_type,
+                "pickup_number": shipment.pickup_number,
+                "pickup_notes": shipment.pickup_notes,
+                "delivery_number": shipment.delivery_number,
+                "delivery_notes": shipment.delivery_notes,
+            },
+            "pickup_facility": {
+                "name": shipment.pickup_facility_name,
+                "address": shipment.origin_address,
+                "scheduling_type": shipment.pickup_scheduling_type,
+                "operating_hours": f"{shipment.pickup_start_time} - {shipment.pickup_end_time}",
+                "contact_person": f"{shipment.pickup_first_name} {shipment.pickup_last_name}",
+                "phone_number": shipment.pickup_phone_number,
+                "email": shipment.pickup_email,
+            },
+            "delivery_facility": {
+                "name": shipment.delivery_facility_name,
+                "address": shipment.destination_address,
+                "scheduling_type": shipment.delivery_scheduling_type,
+                "operating_hours": f"{shipment.delivery_start_time} - {shipment.delivery_end_time}",
+                "contact_person": f"{shipment.delivery_first_name} {shipment.delivery_last_name}",
+                "phone_number": shipment.delivery_phone_number,
+                "email": shipment.delivery_email,
+            },
+        }
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
 @router.post("/spot/loadboard/accept-ftl-shipment", status_code=status.HTTP_202_ACCEPTED) #UnTested
 def accept_spot_ftl_shipment_from_loadboard(
     shipment_data: AssignShipmentRequest,
