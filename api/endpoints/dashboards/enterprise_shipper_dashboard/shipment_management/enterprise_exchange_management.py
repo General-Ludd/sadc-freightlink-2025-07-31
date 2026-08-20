70b41ee90ec7abc7849ba2b529f82df3d2d942da
sadcfreightlink <sadcfreightlink@gmail.com>
2026-08-20 19:10:37 +0000

    Updated tender Fetch information, and bids fetch introduction v1.5

--- api/endpoints/dashboards/enterprise_shipper_dashboard/shipment_management/enterprise_exchange_management.py
+++ api/endpoints/dashboards/enterprise_shipper_dashboard/shipment_management/enterprise_exchange_management.py
@@ -110,6 +110,7 @@ def get_tender_information(
             "tender_scope_routing_information": {
                 "id": tender.id,
                 "publisher_user_id": tender.publisher_user_id,
+                "proposed_rounds": tender.proposed_rounds,
                 "title": tender.tender_title,
                 "scope_description": tender.scope_description,
                 "business_unit": tender.business_unit,
