5250dd6db48f98e6456b436d7b885607509c0faa
sadcfreightlink <sadcfreightlink@gmail.com>
2026-08-20 11:13:20 +0000

    Tenders fetch v1.4

--- models/Exchange/dedicated_ftl_lane.py
+++ models/Exchange/dedicated_ftl_lane.py
@@ -100,7 +100,7 @@ class Lane_Tender_RFQ(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     client_id = Column(Integer, nullable=False)
-    publisher_user_id = Column(Integer, Nullable=False)
+    publisher_user_id = Column(Integer, nullable=False)
     is_sub_tender = Column(Boolean, default=False, nullable=False)
     parent_tender_id = Column(Integer, ForeignKey("ftl_lane_tenders.id"), nullable=True)
 
