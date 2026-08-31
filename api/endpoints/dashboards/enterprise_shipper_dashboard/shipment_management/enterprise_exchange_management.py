e9f8370210edc30e486c0380c1e0da1e967c13a7
sadcfreightlink <sadcfreightlink@gmail.com>
2026-08-31 03:01:51 +0000

    Client load exchanges fetch v1.3

--- api/endpoints/dashboards/enterprise_shipper_dashboard/shipment_management/enterprise_exchange_management.py
+++ api/endpoints/dashboards/enterprise_shipper_dashboard/shipment_management/enterprise_exchange_management.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 from db.database import SessionLocal
 from models.Exchange.auction import Exchange_FTL_Lane_Bid, Exchange_FTL_Shipment_Bid, Exchange_POWER_Shipment_Bid, Lane_Tender_RFQ_Bids
 from models.Exchange.dedicated_ftl_lane import FTL_Lane_Exchange, Lane_Tender_RFQ, Lane_Tender_RFQ_Stop, Lane_Tender_RFQ_Vehicle_Config, Lane_Tender_RFQ_Volume_Profile, Lane_Tender_RFQ_Accessorial
+from models.Exchange.auction import Shipment_Auction_Bid
 from models.shipper import Corporation
 from models.user import Director
 from models.Exchange.ftl_shipment import Client_Shipment_Auction, Client_Shipment_Auction_Stop, Client_Shipment_Auction_Vehicle_Requirement
