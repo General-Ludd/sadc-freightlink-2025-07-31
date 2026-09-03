f75f65d7d0dbe06494c63c0636766ad90a543b78
sadcfreightlink <sadcfreightlink@gmail.com>
2026-09-03 11:56:31 +0000

    Loadboard Shipment Auction Fix place bid.v3.3

--- schemas/exchange_bookings/auction.py
+++ schemas/exchange_bookings/auction.py
@@ -100,7 +100,6 @@ class Create_Tender_Bid(BaseModel):
     bid_notes: Optional [str] = None
 
 class Create_Shipment_Bid(BaseModel):
-    auction_id: int
     rate: float
     number_of_loads: int
     lead_time: str
