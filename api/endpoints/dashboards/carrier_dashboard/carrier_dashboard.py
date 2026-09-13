903efd2b69b4a8274aa74db307840c158bd6cbd8
sadcfreightlink <sadcfreightlink@gmail.com>
2026-09-13 19:34:27 +0000

    Carrier Dashboard Update v3.8.4.11

--- api/endpoints/dashboards/carrier_dashboard/carrier_dashboard.py
+++ api/endpoints/dashboards/carrier_dashboard/carrier_dashboard.py
@@ -625,12 +625,33 @@ def get_carrier_disputes(
                 "filed": d.created_at,
                 "claim_amount": None or "Not Applicable",
                 "details": d.additional_details,
-
+                "resolvment_status": d.conclusion if d.conclusion else None
             })
 
         return {
             "disputes": dispute_data
         }
-    except Exception as e:
-        print(f"Error retrieving carrier account information: {str(e)}")
-        return {""error": str(e)}
+        return {
+            "disputes": dispute_data
+        }
+
+@router.patch("/carrier/update-banking-details", status_code=status.HTTP_201_CREATED) #UnTested
+def partial_update_carrier_financial_account(
+    financial_data: CarrierFinancialAccountUpdate,
+    db: Session = Depends(get_db),
+    current_user: dict = Depends(get_current_user)
+):
+    company_id = current_user.get("company_id")
+    financial_account = db.query(CarrierFinancialAccounts).filter(
+        CarrierFinancialAccounts.id == company_id
+    ).first()
+
+    if not financial_account:
+        raise HTTPException(status_code=404, detail="Financial Account not found or not authorized")
+
+    for key, value in financial_data.dict(exclude_unset=True).items():
+        setattr(financial_account, key, value)
+
+    db.commit()
+    db.refresh(financial_account)
+    return financial_account
