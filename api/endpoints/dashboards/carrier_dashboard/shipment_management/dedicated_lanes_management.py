6fd1b60c21c54c88806ef4268702db0e429c7d44
sadcfreightlink <sadcfreightlink@gmail.com>
2026-09-14 19:43:18 +0000

    Carrier Dashboard Update v3.8.7

--- api/endpoints/dashboards/carrier_dashboard/shipment_management/dedicated_lanes_management.py
+++ api/endpoints/dashboards/carrier_dashboard/shipment_management/dedicated_lanes_management.py
@@ -6,7 +6,7 @@ from models.Exchange.auction import Exchange_FTL_Shipment_Bid, Exchange_POWER_Sh
 from models.brokerage.assigned_lanes import Assigned_Ftl_Lanes
 from models.brokerage.assigned_shipments import Assigned_Power_Shipments, Assigned_Spot_Ftl_Shipments
 from models.brokerage.finance import CarrierFinancialAccounts, Lane_Interim_Invoice, Load_Invoice
-from models.borkerage.loadboard import Lane_Tender_Loadboard
+from models.brokerage.loadboard import Lane_Tender_Loadboard
 from models.carrier import Carrier
 from models.spot_bookings.ftl_shipment import FTL_SHIPMENT, FTL_Shipment_Docs
 from models.spot_bookings.dedicated_lane_ftl_shipment import Lane_Stop, Lane_Vehicle_Config, Lane_Volume_Profile
