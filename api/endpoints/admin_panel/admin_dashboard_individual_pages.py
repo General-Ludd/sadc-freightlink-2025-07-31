52140ab60499486c08958bb590f8cf2a9d2210c1
sadcfreightlink <sadcfreightlink@gmail.com>
2026-09-15 17:59:23 +0000

    Admin Dashboard Shipper Fetch Update v1.3

--- api/endpoints/admin_panel/admin_dashboard_individual_pages.py
+++ api/endpoints/admin_panel/admin_dashboard_individual_pages.py
@@ -210,7 +210,9 @@ def admin_get_shipper_company_id(
                 db.query(Client_Shipment_Auction_Vehicle_Requirement)
                 .filter(
                     Client_Shipment_Auction_Vehicle_Requirement.auction_id
-                    == auction.id
+                    == auction,
+                    Client_Shipment_Auction_Vehicle_Requirement.stop_type
+                    == "Primary"
                 )
                 .all()
             )
